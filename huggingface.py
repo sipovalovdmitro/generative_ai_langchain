6796a0ea6337c18bfa05da416361f6b0a24bbd09
sipovalovdmitro <sipovalovdmitro@gmail.com>
2023-05-17 09:20:20 +0300

    init

---
+++ huggingface.py
@@ -0,0 +1,34 @@
+import os
+from langchain import PromptTemplate, HuggingFaceHub, LLMChain
+
+os.environ['HUGGINGFACEHUB_API_TOKEN'] = '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'
+
+flan_t5 = HuggingFaceHub(
+    repo_id="google/flan-t5-xl",
+    model_kwargs={"temperature":1e-10}
+)
+
+template = """Question: {question}
+
+Answer: """
+
+prompt = PromptTemplate(template=template, input_variables=['question'])
+
+llm_chain = LLMChain(
+    prompt=prompt,
+    llm=flan_t5
+)
+
+question = "Which NFL team won the Super Bowl in the 2010 season?"
+
+print(llm_chain.run(question))
+
+qs=[
+    {'question':"Which NFT team won the Super Bowl in the 2010 season?"},
+    {'question':"If I am 6 ft 4 inches, how tall am I in centimeters?"},
+    {'question':"Who was the 12th person on the moon?"},
+    {'question':"How many eyes does a blade of grass have?"}
+]
+
+res = llm_chain.generate(qs)
+print(res)
